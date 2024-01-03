93772dd67e07f751b723bfe80f29460422e26f10
Aryan Kumar Yadav <ay3978943@gmail.com>
2024-01-03 09:10:12 +0530

    php

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+echo "hello world hhh";
+//single line docs
+
+/* 
+jgjg]g
+gggkg
+gglgog
+gggkg
+
+
+*/
+?>
+
+<!-- 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php tutorial</title>
+</head>
+<body>
+    <h1><?php echo "Hello world" ?></h1>
+
+    <?php 
+    echo "<h2>how  r u </h2>";
+    ?>
+</body>
+</html> -->
+
+
+<!-- variable and datatypes    content -->
+
+
